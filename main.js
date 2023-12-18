0f4d4485e2bfaa60ad60ef261b0a462499cadc18
Ahmad Shahzad <146444891+Ahmad-06@users.noreply.github.com>
2023-12-18 10:04:30 +0000

    ggvd

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('./index.html'); 
+    res.sendFile('./index.html'); 
 });
 
 app.all('/answer', (req, res) => {
